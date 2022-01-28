4356f0cacbcfdeddbd90cc4ed16760f4e4e959a1
Braeden Sowinski <sowinskibraeden@gmail.com>
2022-01-28 13:17:12 -0800

    update/custom-dev-prefix

--- bot.js
+++ bot.js
@@ -298,6 +298,7 @@ client.on('ready', () => {
 // Basic Commands
 client.on('message', async (message) => {
   let prefix = "?";
+ 	if (process.env.DEBUG=="true") prefix = "dev?";
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
   const args = message.content.slice(prefix.length).trim().split(' ');
